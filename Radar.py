25624ae589dfa8f40a605a825ed03b2c9390478c
Gabriel Gasque <116985477+gaxque@users.noreply.github.com>
2023-09-02 04:28:40 -0300

    Update-Radar.py

--- Radar.py
+++ Radar.py
@@ -3,7 +3,7 @@
 velocidade = float(input('Qual a velocidade atual? '))
 
 if velocidade > 60:
-    multa = (velocidade-60) * 7
+    multa = (velocidade-60) * 7 #7 Reais a cada Km ultrapassado
     print('Você está acima da velocidade e sua multa será de: R${:.2f}' .format(multa))
 else:
     print ('Você está dentro da velocidade permitida - Dirija com cuidado')
