f4d117ef006b9b16a72b0ad5a9dec4b4f41f0055
Gabriel Gasque <116985477+gaxque@users.noreply.github.com>
2023-09-02 04:24:04 -0300

    Ano-Bissexto.py

--- Ano-Bissexto.py
+++ Ano-Bissexto.py
@@ -1,12 +1,12 @@
-#Ano bissexto / Como pegar o ano do meu pc com python
+#Ano bissexto 
+
 
 from datetime import date
-
-
-#
 ano = int(input('Digite um ano, ou coloque o ano atual digitando 0 (zero): '))
+
 if ano == 0:
-    ano = date.today().year
+    ano = date.today().year. #Pega o ano do pc do usuário 
+    
 if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
     print('O {} é BISSEXTO!'.format(ano))
 else:
