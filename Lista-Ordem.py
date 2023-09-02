d8f45805906583be34a5e990847e75238094b357
Gabriel Gasque <116985477+gaxque@users.noreply.github.com>
2023-09-02 04:27:35 -0300

    Lista-Ordem.py

--- Lista-Ordem.py
+++ Lista-Ordem.py
@@ -5,6 +5,6 @@ n2 = str(input('Segundo: '))
 n3 = str(input('Terceiro: '))
 n4 = str(input('Quarta: '))
 lista = [n1, n2, n3, n4]
-shuffle(lista)
+shuffle(lista) #Embaralha 
 print('A ordem de pessoas será : ')
 print(lista)
